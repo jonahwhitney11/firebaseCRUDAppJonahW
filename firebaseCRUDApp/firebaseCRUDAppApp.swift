105787ed0d1429c1ec89e5190faed998d1013d80
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-04-01 13:28:25 -0500

    Initial Commit

---
+++ firebaseCRUDApp/firebaseCRUDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseCRUDAppApp.swift
+//  firebaseCRUDApp
+//
+//  Created by Jonah Whitney on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct firebaseCRUDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
